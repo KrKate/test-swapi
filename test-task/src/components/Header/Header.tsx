da6b0894c5218926b5253fb661cd6c96c11036dd
Ekaterina Kiryanova <109188496+KrKate@users.noreply.github.com>
2024-01-24 01:38:01 +0500

    feat: add a request to SWAPI

--- test-task/src/components/Header/Header.tsx
+++ test-task/src/components/Header/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './Header.module.css';
 import Search from '../Search/Search';
+import { searchCharacter } from '../../api/api';
 
 class Header extends React.Component {
+  handleSearch = (characterName: string) => {
+    searchCharacter(characterName)
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  };
   render() {
     return (
       <header className={styles.header}>
-        <Search />
+        <Search onSearch={this.handleSearch} />
       </header>
     );
   }
--- test-task/src/components/Search/Search.tsx
+++ test-task/src/components/Search/Search.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import styles from './Search.module.css';
 
-class Search extends React.Component {
+interface SearchProps {
+  onSearch: (characterName: string) => void;
+}
+
+interface SearchState {
+  searchValue: string;
+}
+class Search extends React.Component<SearchProps, SearchState> {
+  constructor(props: SearchProps) {
+    super(props);
+    this.state = {
+      searchValue: '',
+    };
+  }
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchValue: e.target.value });
+  };
+
+  handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    this.props.onSearch(this.state.searchValue);
+  };
+
   render() {
     return (
-      <form className={styles.searchContainer}>
-        <input placeholder="Enter character name" type="text" />
-        <button className={styles.searchButton}>Search</button>
+      <form className={styles.searchContainer} onSubmit={this.handleSubmit}>
+        <input
+          placeholder="Enter character name"
+          type="text"
+          value={this.state.searchValue}
+          onChange={this.handleChange}
+        />
+        <button className={styles.searchButton} type="submit">
+          Search
+        </button>
       </form>
     );
   }
